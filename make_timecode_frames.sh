b0131cdc85429331ef34553222f5fb9a2920bb57
Adam Schuster <adam@atompowered.net>
2025-05-18 14:46:08 -0700

    update to 10fps

--- make_timecode_frames.sh
+++ make_timecode_frames.sh
@@ -4,9 +4,9 @@ ffmpeg -stream_loop -1 -framerate 25 -pattern_type glob -i "../cam1/*.png" \
 	-c:v libx264 \
   	-preset veryfast \
   	-tune zerolatency \
-  	-x264-params "bframes=0:keyint=25" \
+  	-x264-params "bframes=0:keyint=10" \
   	-b:v 512k \
-	-vf "scale=1920:1080:force_original_aspect_ratio=decrease,fps=25,format=yuv420p,drawtext=fontfile=/opt/SpaceMonoNerdFontMono-Regular.ttf:text='%{pts\:hms}':fontcolor=white:fontsize=64:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)-20:y=(h-text_h)-20" \
+	-vf "scale=1920:1080:force_original_aspect_ratio=decrease,fps=10,format=yuv420p,drawtext=fontfile=/opt/SpaceMonoNerdFontMono-Regular.ttf:text='%{pts\:hms}':fontcolor=white:fontsize=64:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)-20:y=(h-text_h)-20" \
 	-f image2 \
 	-t 60 \
 	'./cam1/frame-%4d.h264'
--- make_timecode_video.sh
+++ make_timecode_video.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ffmpeg -stream_loop -1 -framerate 25 -pattern_type glob -i "../cam2/*.png" \
+ffmpeg -stream_loop -1 -framerate 25 -pattern_type glob -i "../cam1/*.png" \
 	-c:v libx264 \
-	-vf "scale=1024:768:force_original_aspect_ratio=decrease,fps=25,format=yuv420p,drawtext=fontfile=/opt/SpaceMonoNerdFontMono-Regular.ttf:text='%{pts\:hms}':fontcolor=white:fontsize=64:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)-20:y=(h-text_h)-20" \
+	-vf "scale=1024:768:force_original_aspect_ratio=decrease,fps=10,format=yuv420p,drawtext=fontfile=/opt/SpaceMonoNerdFontMono-Regular.ttf:text='%{pts\:hms}':fontcolor=white:fontsize=64:box=1:boxcolor=black@0.5:boxborderw=5:x=(w-text_w)-20:y=(h-text_h)-20" \
 	-t 60 \
-	cam2.mp4
+	cam1.mp4
